8f83b1ecd619fc598f65c46a1d3198294e848baf
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-10-16 22:13:26 +0100

    remove trailing whitespace

--- classificationModel.py
+++ classificationModel.py
@@ -27,10 +27,10 @@ def buildStopWords():
         stopwords.remove(word)
 
     stopwords += extras
-    
+
     return stopwords
-    
-    
+
+
 def getLabelsQuestions(file):
     labels = []
     questions = []
@@ -40,17 +40,17 @@ def getLabelsQuestions(file):
         labels.append(treated[0])
         questions.append(treated[1][:-1])
 
-    file.close()   
+    file.close()
     return questions, labels
 
 def getListFromFile(file):
     result = []
     for line in file:
         result.append(line)
-        
+
     return result
 
-def cleanQuestions(questions, stopwords):    
+def cleanQuestions(questions, stopwords):
     # expressions are joined with sym
     def groupExpression(question, sym):
         result = [question[0]]           # first word is always capital
@@ -80,7 +80,7 @@ def cleanQuestions(questions, stopwords):
             tokens[i] = tokens[i].lower()
 
         return tokens
-    
+
     def nltk_tag_to_wordnet_tag(nltk_tag):
         wordnet_tags = {'J': wordnet.ADJ, 'V': wordnet.VERB, 'N': wordnet.NOUN, 'R': wordnet.ADV}
         if nltk_tag[0] in wordnet_tags:
@@ -101,7 +101,7 @@ def cleanQuestions(questions, stopwords):
 
     sym = '|'
     lemmatizer = WordNetLemmatizer()
-    
+
     questions_tokens = []
     for question in questions:
         text_tokens = word_tokenize(question)
@@ -114,14 +114,14 @@ def cleanQuestions(questions, stopwords):
     clean_questions = []
     for question in questions_tokens:
         clean_questions.append(' '.join(question))
-        
+
     return clean_questions  
 
 def getCoarseLabels(labels):
     coarse = []
     for label in labels:
         coarse.append(label.split(':')[0])
-    
+
     return coarse
 
 
@@ -135,51 +135,51 @@ def preProcessDataSet(is_coarse, trainFile, testQuestionsFile, testLabelsFile):
     train_x, train_y = getLabelsQuestions(train_f)
     test_x = getListFromFile(testQ_f)
     test_y = getListFromFile(testL_f)
-    
+
     stopwords = buildStopWords()
     train_x = cleanQuestions(train_x, stopwords)
     test_x = cleanQuestions(test_x, stopwords)
-    
+
     if is_coarse:
         train_y = getCoarseLabels(train_y)
         test_y = getCoarseLabels(test_y)
-    
+
     encoder = LabelEncoder()
     train_y = encoder.fit_transform(train_y)
     test_y = encoder.fit_transform(test_y)
-    
+
     tfidf_vect = TfidfVectorizer(use_idf=True)
     train_x_tfidf = tfidf_vect.fit_transform(train_x)
     test_x_tfidf = tfidf_vect.transform(test_x)
-    
+
     #print('train_y = ', train_y.shape)
     #print('test_y = ', test_y.shape)
     #print('train_x_tfidf = ', train_x_tfidf.shape)
     #print('test_x_tfidf = ', test_x_tfidf.shape)
     #print(tfidf_vect.vocabulary_)
-    
+
     return train_x_tfidf, train_y, test_x_tfidf, test_y
 
 def useSVM(train_x_tfidf, train_y, test_x_tfidf, test_y):
     # training phase
     SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
     SVM.fit(train_x_tfidf, train_y)
-    
+
     # prediction phase
     predictions_SVM = SVM.predict(test_x_tfidf)
-    
+
     # accuracy
     print("SVM Accuracy Score -> ", accuracy_score(predictions_SVM, test_y)*100)
-   
+
 
 def useNB(train_x_tfidf, train_y, test_x_tfidf, test_y):
     # training phase
     Naive = naive_bayes.MultinomialNB()
     Naive.fit(train_x_tfidf, train_y)
-    
+
     # prediction phase
     predictions_NB = Naive.predict(test_x_tfidf)
-    
+
     # accuracy
     print("Naive Bayes Accuracy Score -> ", accuracy_score(predictions_NB, test_y)*100)
 
@@ -187,13 +187,13 @@ def useModels(is_coarse, use_svm, use_nb, train, devQ, devL):
     train_x_tfidf, train_y, test_x_tfidf, test_y = preProcessDataSet(is_coarse, train, devQ, devL)
     if use_svm:
         useSVM(train_x_tfidf, train_y, test_x_tfidf, test_y)
-        
+
     if use_nb:
         useNB(train_x_tfidf, train_y, test_x_tfidf, test_y) 
-        
+
 def questionClassification(is_coarse, use_svm=False, use_nb=True, train='TRAIN.txt', devQ='DEV-questions.txt', devL='DEV-labels.txt'):
     useModels(is_coarse, use_svm, use_nb, train, devQ, devL)
-    
+
 
 
 # In[3]:
